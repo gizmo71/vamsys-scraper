2e2c36cd89c836751bd787a759af39e95e93092d
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2023-02-17 19:29:06 +0000

    Working skeleton!

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vamsys-scraper
 
-In WSL, apt install python3 python3-pip.
+In WSL, `apt install python3 python3-pip chromium`
 
-Then pip install selenium==4.4.3 && pip install webdriver-manager
+Then `pip install selenium==4.4.3 && pip install webdriver-manager`
 
--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 
-driverManager = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM)
-driver = driverManager.install()
-service = ChromiumService(driver)
-driver = webdriver.Chrome(service)
+driver = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+driver = webdriver.Chrome(service = ChromiumService(driver))
+
+driver.quit()
