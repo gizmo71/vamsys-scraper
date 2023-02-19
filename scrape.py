3f82b60ba80194d7c1a47022264c5edfc5e9f137
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2023-02-19 12:19:09 +0000

    Tweak timing and save all results in a single file.

--- scrape.py
+++ scrape.py
@@ -1,3 +1,5 @@
+import atexit
+import json
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,7 +9,6 @@ from webdriver_manager.core.utils import ChromeType
 from seleniumwire import webdriver
 from seleniumwire.utils import decode as sw_decode
 from time import sleep
-import atexit
 
 driver_manager = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
 driver = webdriver.Chrome(service=ChromiumService(driver_manager), seleniumwire_options={'request_storage': 'memory'})
@@ -43,20 +44,25 @@ def handle_destinations(driver):
             map = body
     return '{\n"airline":%s,\n"map":%s\n}' % (airline, map) if airline and map else None
 
+all_data = []
+
 # Or something from https://seleniumhq.github.io/selenium/docs/api/py/webdriver_support/selenium.webdriver.support.expected_conditions.html?highlight=expected
 pilot_id_elements = WebDriverWait(driver, 5).until(lambda d: d.find_elements(by=By.XPATH, value="//div[.//p[text()='PIREPs Filed']]/dl/dd/div/button[./i[@class='fal fa-plane-departure']]"))
-for pilot_id in reversed(list(map(lambda e: e.text, pilot_id_elements))): # Convert to a list so it doesn't hang onto the elements for too long.
+for pilot_id in list(map(lambda e: e.text, pilot_id_elements)): # Convert to a list so it doesn't hang onto the elements for too long.
     print(pilot_id)
     xpath = f"//button[normalize-space() = '{pilot_id}']//ancestor::div[2]"
-    pilot_id_element = WebDriverWait(driver, 2).until(lambda d: d.find_element(by=By.XPATH, value=xpath))
+    pilot_id_element = WebDriverWait(driver, 10).until(lambda d: d.find_element(by=By.XPATH, value=xpath))
     driver.execute_script("arguments[0].scrollIntoView();", pilot_id_element) # Seems to scroll it too far up - is it the wrong element to do this to? Perhaps just run the "login(nnn)" javascript directly?
     pilot_id_element.click()
 
-    sleep(2)
+    sleep(5)
     del driver.requests
     driver.get("https://vamsys.io/destinations")
-    airline_and_map = WebDriverWait(driver, 5).until(handle_destinations)
-    print(airline_and_map)
+    airline_and_map = WebDriverWait(driver, 15).until(handle_destinations)
+    all_data.append(json.loads(airline_and_map))
 
-    sleep(2)
+    sleep(5)
     driver.get("https://vamsys.io/select") # Back to the airline selection page for the next airline.
+
+with open(f'vamsys.json', 'w', encoding="utf-8") as f:
+    json.dump(all_data, f, indent=4)
