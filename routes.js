e46a68152ad62fc25712f33fbcc09b4482a7c133
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2023-02-24 20:46:14 +0000

    Click to reverse in/out choice.

--- routes.js
+++ routes.js
@@ -7,22 +7,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 const shown = [];
 
+var currentIcao = "-none-";
+var isInbound = false;
+
 function airportClicked(e) {
     var icao = e.target.options.icao;
+    if (icao == currentIcao)
+        isInbound = !isInbound;
+    else
+        currentIcao = icao;
     while (shown.length) shown.pop().removeFrom(map);
     for (route in routes) {
         const from = route.substring(0, 4);
         const to = route.substring(5, 9);
-        if (icao != to && icao != from) continue;
         var colour = undefined, tooltip = Object.getOwnPropertyNames(routes[route].type_to_airlines).map(type => type + ": " + routes[route].type_to_airlines[type].join(', ')).join('</br>') + ' ';
-        if (routes.hasOwnProperty(to + "-" + from)) {
-            if (to == icao) continue; // Only do one copy of the route
-            colour = "purple", tooltip += "to/from " + to;
-        } else if (from != icao) {
+        if (from == icao && !isInbound) {
             colour = "blue"; tooltip += "from " + from;
-        } else if (to != icao) {
+        } else if (to == icao && isInbound) {
             colour = "red"; tooltip += "to " + to;
-        }
+        } else
+            continue;
         var polyline = new L.Geodesic([airports[from].latlng, airports[to].latlng], {opacity: 0.666, color: colour}).bindTooltip(tooltip);
         shown.push(polyline);
         polyline.setText((colour != "purple" ? '' :   '◄') + routes[route].distance + '►', {repeat: true, attributes: {fill: colour}});
@@ -30,9 +34,11 @@ function airportClicked(e) {
     }
 }
 
+const airportIcon = L.divIcon({html: "\u1F6EB;\u1F6EC;"});
 for (icao in airports) {
     const airport = airports[icao];
-    L.marker(airport.latlng, {icao: icao}).addTo(map)
+    const icon = L.divIcon({html: "\u2708;</br>" + airport['iata'], className: 'airport'});
+    airport.marker = L.marker(airport.latlng, {icao: icao, icon: icon}).addTo(map)
         .bindTooltip("<b>" + icao + "</b><br/>" + airport.names.join('<br/>'))
         .on('click', airportClicked);
 }
