a0f00d26061e451956cd33a0dc9a0e690079dbfb
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2023-12-26 09:41:28 +0000

    Add airline callsigns.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -1,124 +1,261 @@
 const airlines = {
     "1": {
+        "callsigns": [
+            "EJU",
+            "EZS",
+            "EZY"
+        ],
         "name": "EasyJet",
         "rank_info": "Senior First Officer:\n\t+90.3 Hours (53.4 PIREPs)\n\t+4075.0 Points (21.6 PIREPs)\nCaptain:\n\t+390.3 Hours (230.8 PIREPs)\n\t+36575.0 Points (193.6 PIREPs)\nTraining Captain:\n\t+790.3 Hours (467.3 PIREPs)\n\t+76575.0 Points (405.3 PIREPs)\nBase Captain:\n\t+1790.3 Hours (1058.6 PIREPs)\n\t+176575.0 Points (934.7 PIREPs)\nTime mode: block",
         "sortName": "EasyJet"
     },
     "3": {
+        "callsigns": [
+            "RUK",
+            "RYR"
+        ],
         "name": "Ryanair",
         "rank_info": "Bonus points count towards normal points total\nFirst Officer:\n\t+116.3 Hours (94.1 PIREPs)\n\t+12568.0 Points (40.5 PIREPs)\nType Rating Instructor:\n\t+306.3 Hours (247.9 PIREPs)\n\t+33168.0 Points (106.8 PIREPs)\nCaptain:\n\t+596.3 Hours (482.6 PIREPs)\n\t+65068.0 Points (209.4 PIREPs)\nLine Training Captain:\n\t+846.3 Hours (685.0 PIREPs)\n\t+92568.0 Points (298.0 PIREPs)\nType Rating Examiner:\n\t+1196.3 Hours (968.3 PIREPs)\n\t+131068.0 Points (421.9 PIREPs)\nTime mode: air",
         "sortName": "Ryanair"
     },
     "9": {
+        "callsigns": [
+            "EWE",
+            "EWG",
+            "GWI",
+            "OCN"
+        ],
         "name": "Eurowings",
         "rank_info": "Bonus points not included in normal points total\nSenior First Officer:\n\t+384.7 Hours (216.9 PIREPs)\n\t+28353.0 Points (191.0 PIREPs)\nCaptain:\n\t+884.7 Hours (498.8 PIREPs)\n\t+66353.0 Points (447.1 PIREPs)\n\t+14487.0 Bonus Points (60.7 PIREPs)\nSenior Captain:\n\t+1384.7 Hours (780.8 PIREPs)\n\t+104353.0 Points (703.1 PIREPs)\n\t+29487.0 Bonus Points (123.6 PIREPs)\nInstructor:\n\t+4884.7 Hours (2754.2 PIREPs)\n\t+370353.0 Points (2495.4 PIREPs)\nFlying Legend:\n\t+9884.7 Hours (5573.5 PIREPs)\n\t+752353.0 Points (5069.2 PIREPs)\nTime mode: air",
         "requirements": "1 PIREP(s) required over 180 days\nNext 1 PIREP(s) required by 2024-06-09",
         "sortName": "Eurowings"
     },
     "13": {
+        "callsigns": [
+            "CFG",
+            "TCW",
+            "TCX",
+            "VKG"
+        ],
         "name": "vTCXgroup",
         "rank_info": "Bonus points count towards normal points total\nFirst Officer:\n\t+61.5 Hours (33.6 PIREPs)\n\t+115.0 Points (0.4 PIREPs)\nSenior First Officer:\n\t+711.5 Hours (388.3 PIREPs)\n\t+1615.0 Points (5.7 PIREPs)\nCaptain:\n\t+961.5 Hours (524.7 PIREPs)\n\t+6115.0 Points (21.5 PIREPs)\nTraining Captain:\n\t+4461.5 Hours (2434.6 PIREPs)\n\t+27015.0 Points (94.8 PIREPs)\nChief Pilot:\n\t+4461.5 Hours (2434.6 PIREPs)\n\t+84015.0 Points (294.8 PIREPs)\nTime mode: air",
         "sortName": "Thomas Cook"
     },
     "30": {
+        "callsigns": [
+            "EAG",
+            "EAI",
+            "EIN",
+            "EUK"
+        ],
         "name": "Aer Lingus",
         "rank_info": "Bonus points count towards normal points total\nFirst Officer:\n\t+7.6 Hours (5.4 PIREPs)\n\t+120.0 Points (0.4 PIREPs)\nSenior First Officer:\n\t+77.6 Hours (55.3 PIREPs)\n\t+7620.0 Points (27.8 PIREPs)\n\t+200.0 Bonus Points (4.0 PIREPs)\nCaptain:\n\t+277.6 Hours (198.0 PIREPs)\n\t+25620.0 Points (93.6 PIREPs)\n\t+3200.0 Bonus Points (64.0 PIREPs)\nSenior Captain (TRE/TRI/Line):\n\t+877.6 Hours (626.1 PIREPs)\n\t+70620.0 Points (258.0 PIREPs)\n\t+11200.0 Bonus Points (224.0 PIREPs)\nBase Chief Pilot:\n\t+1477.6 Hours (1054.1 PIREPs)\n\t+125620.0 Points (458.9 PIREPs)\n\t+19200.0 Bonus Points (384.0 PIREPs)\nTime mode: air",
         "requirements": "1 PIREP(s) required over 30 days\nNext 1 PIREP(s) required by 2024-01-21",
         "sortName": "Linugs"
     },
     "35": {
+        "callsigns": [
+            "BAW",
+            "CFE",
+            "EFW",
+            "QTR",
+            "SHT"
+        ],
         "name": "British Airways",
         "rank_info": "Bonus points count towards normal points total\nFirst Officer:\n\t+83.7 Hours (56.7 PIREPs)\n\t+6334.0 Points (31.3 PIREPs)\nSenior First Officer:\n\t+258.7 Hours (175.2 PIREPs)\n\t+21834.0 Points (107.9 PIREPs)\nCaptain:\n\t+558.7 Hours (378.3 PIREPs)\n\t+54334.0 Points (268.5 PIREPs)\nTraining Captain:\n\t+958.7 Hours (649.2 PIREPs)\n\t+94334.0 Points (466.2 PIREPs)\nBase Captain:\n\t+1958.7 Hours (1326.4 PIREPs)\n\t+194334.0 Points (960.4 PIREPs)\nTime mode: block",
         "sortName": "British Airways"
     },
     "42": {
+        "callsigns": [
+            "WAZ",
+            "WMT",
+            "WUK",
+            "WZZ"
+        ],
         "name": "Wizz",
         "rank_info": "Bonus points not included in normal points total\nSenior First Officer:\n\t+78.3 Hours (44.8 PIREPs)\n\t+4370.0 Points (22.0 PIREPs)\nCaptain:\n\t+228.3 Hours (130.6 PIREPs)\n\t+16870.0 Points (85.1 PIREPs)\nTraining Captain:\n\t+528.3 Hours (302.1 PIREPs)\n\t+51870.0 Points (261.6 PIREPs)\nBase Captain:\n\t+1928.3 Hours (1102.7 PIREPs)\n\t+191870.0 Points (967.6 PIREPs)\nTime mode: air",
         "sortName": "Wizz"
     },
     "46": {
+        "callsigns": [
+            "ASH",
+            "GJS",
+            "RPA",
+            "SKW",
+            "UAL",
+            "UCA"
+        ],
         "name": "United",
         "rank_info": "Bonus points count towards normal points total\nFirst Officer:\n\t+12.0 Hours (10.6 PIREPs)\n\t+98.0 Points (0.4 PIREPs)\nSenior First Officer:\n\t+32.0 Hours (28.2 PIREPs)\n\t+4098.0 Points (15.1 PIREPs)\nCaptain:\n\t+92.0 Hours (80.9 PIREPs)\n\t+8098.0 Points (29.8 PIREPs)\n\t+350.0 Bonus Points (3.8 PIREPs)\nBase Captain:\n\t+142.0 Hours (124.9 PIREPs)\n\t+13098.0 Points (48.2 PIREPs)\n\t+850.0 Bonus Points (9.2 PIREPs)\nChief Pilot:\n\t+292.0 Hours (256.8 PIREPs)\n\t+28098.0 Points (103.4 PIREPs)\n\t+1350.0 Bonus Points (14.5 PIREPs)\nTime mode: block",
         "sortName": "United"
     },
     "55": {
+        "callsigns": [
+            "NKS"
+        ],
         "name": "Spirit",
         "rank_info": "Bonus points not included in normal points total\nFirst Officer:\n\t+68.9 Hours (37.7 PIREPs)\n\t+6350.0 Points (29.6 PIREPs)\nCaptain:\n\t+243.9 Hours (133.5 PIREPs)\n\t+18850.0 Points (87.8 PIREPs)\n\t+2770.0 Bonus Points (15.8 PIREPs)\nSenior Captain:\n\t+718.9 Hours (393.5 PIREPs)\n\t+71350.0 Points (332.3 PIREPs)\n\t+13020.0 Bonus Points (74.3 PIREPs)\nTime mode: air",
         "requirements": "1 PIREP(s) required over 90 days\nNext 1 PIREP(s) required by 2024-02-05",
         "sortName": "Spirit"
     },
     "64": {
+        "callsigns": [
+            "DLH",
+            "GEC"
+        ],
         "name": "Lufthansa",
         "rank_info": "Bonus points count towards normal points total\nCaptain:\n\t+443.5 Hours (290.4 PIREPs)\n\t+2542.0 Points (27.2 PIREPs)\nTraining Captain:\n\t+443.5 Hours (290.4 PIREPs)\n\t+31542.0 Points (337.5 PIREPs)\nBase Captain:\n\t+1443.5 Hours (945.3 PIREPs)\n\t+71542.0 Points (765.5 PIREPs)\nTime mode: air",
         "sortName": "Lufthansa"
     },
     "227": {
+        "callsigns": [
+            "QTR"
+        ],
         "name": "Qatar",
         "rank_info": "Second Officer:\n\t+47.9 Hours (46.2 PIREPs)\n\t+2570.0 Points (12.0 PIREPs)\n\t+200.0 Bonus Points\nFirst Officer:\n\t+197.9 Hours (190.7 PIREPs)\n\t+4570.0 Points (21.3 PIREPs)\n\t+1500.0 Bonus Points\nSenior First Officer:\n\t+397.9 Hours (383.5 PIREPs)\n\t+8570.0 Points (39.9 PIREPs)\n\t+2000.0 Bonus Points\nCaptain:\n\t+597.9 Hours (576.2 PIREPs)\n\t+11570.0 Points (53.8 PIREPs)\n\t+3500.0 Bonus Points\nTraining Captain:\n\t+997.9 Hours (961.7 PIREPs)\n\t+99570.0 Points (463.1 PIREPs)\n\t+4000.0 Bonus Points\nBase Captain:\n\t+1997.9 Hours (1925.5 PIREPs)\n\t+199570.0 Points (928.2 PIREPs)\n\t+5000.0 Bonus Points\nTime mode: block",
         "requirements": "1 PIREP(s) required over 30 days\nNext 1 PIREP(s) required by 2024-01-05",
         "sortName": "Qatar"
     },
     "265": {
+        "callsigns": [
+            "ANA",
+            "CAO",
+            "CCA",
+            "CPA"
+        ],
         "name": "Air China",
         "rank_info": "Bonus points not included in normal points total\nSecond Officer / \u7b2c\u4e8c\u526f\u9a7e\u9a76:\n\t+36.7 Hours (24.7 PIREPs)\n\t+3005.0 Points (27.2 PIREPs)\nFirst Officer / \u7b2c\u4e00\u526f\u9a7e\u9a76:\n\t+136.7 Hours (92.2 PIREPs)\n\t+13005.0 Points (117.6 PIREPs)\nSenior First Officer / \u8d44\u6df1\u526f\u9a7e\u9a76:\n\t+286.7 Hours (193.3 PIREPs)\n\t+26505.0 Points (239.7 PIREPs)\nCaptain / \u673a\u957f:\n\t+586.7 Hours (395.6 PIREPs)\n\t+59005.0 Points (533.7 PIREPs)\nTraining Captain / \u98de\u884c\u6559\u5458:\n\t+986.7 Hours (665.4 PIREPs)\n\t+99005.0 Points (895.5 PIREPs)\nFleet Captain / \u98de\u884c\u5927\u961f\u961f\u957f:\n\t+1986.7 Hours (1339.7 PIREPs)\n\t+199005.0 Points (1800.0 PIREPs)\nTime mode: block",
         "sortName": "China"
     },
     "291": {
+        "callsigns": [
+            "JBU"
+        ],
         "name": "JetBlue",
         "rank_info": "Bonus points count towards normal points total\nFirst Officer:\n\t+41.5 Hours (43.8 PIREPs)\n\t+2075.0 Points (9.7 PIREPs)\nSenior First Officer:\n\t+191.5 Hours (202.1 PIREPs)\n\t+15075.0 Points (70.5 PIREPs)\nCaptain:\n\t+491.5 Hours (518.7 PIREPs)\n\t+43075.0 Points (201.4 PIREPs)\nTraining Captain:\n\t+991.5 Hours (1046.4 PIREPs)\n\t+78075.0 Points (365.0 PIREPs)\nSenior Training Captain:\n\t+1491.5 Hours (1574.2 PIREPs)\n\t+128075.0 Points (598.8 PIREPs)\nChief Pilot:\n\t+2491.5 Hours (2629.6 PIREPs)\n\t+248075.0 Points (1159.8 PIREPs)\nTime mode: block",
         "sortName": "JetBlue"
     },
     "356": {
+        "callsigns": [
+            "AAG",
+            "BCC",
+            "BER",
+            "BRA",
+            "CJA",
+            "CRX",
+            "DAN",
+            "FCA",
+            "GSM",
+            "ISS",
+            "NLY",
+            "ORN",
+            "SAB",
+            "TTN"
+        ],
         "name": "Dan Air",
         "rank_info": "Senior Captain:\n\t+136.5 Hours (76.4 PIREPs)\nBase Captain:\n\t+751.5 Hours (420.6 PIREPs)\nChief Pilot:\n\t+2071.5 Hours (1159.3 PIREPs)\nTime mode: air",
         "sortName": "Dan Air"
     },
     "500": {
+        "callsigns": [
+            "AWC",
+            "BCS",
+            "BIX",
+            "KRH",
+            "LOG",
+            "TMT",
+            "WUK"
+        ],
         "name": "Titan",
         "rank_info": "Bonus points not included in normal points total\nSecond Officer:\n\t+36.3 Hours (13.3 PIREPs)\n\t+2589.0 Points (9.2 PIREPs)\nFirst Officer:\n\t+136.3 Hours (49.9 PIREPs)\n\t+12589.0 Points (44.6 PIREPs)\nSenior First Officer:\n\t+286.3 Hours (104.8 PIREPs)\n\t+26089.0 Points (92.4 PIREPs)\nCaptain:\n\t+586.3 Hours (214.6 PIREPs)\n\t+58589.0 Points (207.6 PIREPs)\nTraining Captain:\n\t+986.3 Hours (361.1 PIREPs)\n\t+98589.0 Points (349.4 PIREPs)\nBase Captain:\n\t+1986.3 Hours (727.1 PIREPs)\n\t+198589.0 Points (703.7 PIREPs)\nTime mode: block",
         "sortName": "Titan"
     },
     "550": {
+        "callsigns": [
+            "SAS",
+            "SZS"
+        ],
         "name": "SAS",
         "rank_info": "Bonus points count towards normal points total\nFirst Officer:\n\t+31.9 Hours (26.5 PIREPs)\n\t+3222.0 Points (17.4 PIREPs)\nSenior First Officer:\n\t+181.9 Hours (151.0 PIREPs)\n\t+12222.0 Points (66.0 PIREPs)\nCaptain:\n\t+381.9 Hours (317.0 PIREPs)\n\t+27222.0 Points (147.0 PIREPs)\n\t+845.0 Bonus Points (81.8 PIREPs)\nSenior Captain:\n\t+781.9 Hours (649.0 PIREPs)\n\t+37222.0 Points (201.0 PIREPs)\n\t+1845.0 Bonus Points (178.5 PIREPs)\nTime mode: block",
         "sortName": "SAS"
     },
     "706": {
+        "callsigns": [
+            "IGO"
+        ],
         "name": "IndiGo",
         "rank_info": "First Officer:\n\t+36.5 Hours (24.4 PIREPs)\nSenior First Officer:\n\t+86.5 Hours (57.8 PIREPs)\n\t+10150.0 Points (49.4 PIREPs)\nCaptain:\n\t+236.5 Hours (158.1 PIREPs)\n\t+28150.0 Points (136.9 PIREPs)\nBase Captain:\n\t+486.5 Hours (325.2 PIREPs)\n\t+58150.0 Points (282.9 PIREPs)\nTime mode: air",
         "sortName": "IndiGo"
     },
     "876": {
+        "callsigns": [
+            "AIC",
+            "AXB",
+            "IAD",
+            "LLR",
+            "VTI"
+        ],
         "name": "Air India",
         "rank_info": "First Officer:\n\t+47.2 Hours (51.1 PIREPs)\nSenior First Officer:\n\t+297.2 Hours (321.7 PIREPs)\n\t+14380.0 Points (69.6 PIREPs)\nCaptain:\n\t+597.2 Hours (646.4 PIREPs)\n\t+29380.0 Points (142.2 PIREPs)\nBase Captain:\n\t+997.2 Hours (1079.3 PIREPs)\n\t+59380.0 Points (287.3 PIREPs)\nTime mode: air",
         "sortName": "India"
     },
     "939": {
+        "callsigns": [
+            "FFT",
+            "VOI"
+        ],
         "name": "Frontier",
         "rank_info": "First Officer:\n\t+46.6 Hours (41.5 PIREPs)\nSenior First Officer:\n\t+246.6 Hours (219.4 PIREPs)\n\t+2484.0 Points (14.4 PIREPs)\nCaptain:\n\t+496.6 Hours (441.8 PIREPs)\n\t+4484.0 Points (26.1 PIREPs)\n\t+5000.0 Bonus Points\nSenior Captain:\n\t+1496.6 Hours (1331.3 PIREPs)\n\t+99484.0 Points (578.4 PIREPs)\n\t+6000.0 Bonus Points\nBase Captain:\n\t+2996.6 Hours (2665.6 PIREPs)\n\t+199484.0 Points (1159.8 PIREPs)\n\t+10000.0 Bonus Points\nTime mode: block",
         "sortName": "Frontier"
     },
     "1005": {
+        "callsigns": [
+            "ANA",
+            "APJ"
+        ],
         "name": "All Nippon",
         "rank_info": "Bonus points not included in normal points total\nSecond Officer:\n\t+41.2 Hours (37.5 PIREPs)\n\t+3503.0 Points (56.4 PIREPs)\nFirst Officer:\n\t+141.2 Hours (128.4 PIREPs)\n\t+13503.0 Points (217.4 PIREPs)\nSenior First Officer:\n\t+291.2 Hours (264.9 PIREPs)\n\t+27003.0 Points (434.7 PIREPs)\nCaptain:\n\t+591.2 Hours (537.8 PIREPs)\n\t+59503.0 Points (957.8 PIREPs)\nTraining Captain:\n\t+991.2 Hours (901.7 PIREPs)\n\t+99503.0 Points (1601.7 PIREPs)\nBase Captain:\n\t+1991.2 Hours (1811.3 PIREPs)\n\t+199503.0 Points (3211.3 PIREPs)\nTime mode: air",
         "sortName": "Nippon"
     },
     "1033": {
+        "callsigns": [
+            "DAL",
+            "REP",
+            "SKW"
+        ],
         "name": "Delta",
         "rank_info": "First Officer:\n\t+40.0 Hours (36.1 PIREPs)\nSenior First Officer:\n\t+90.0 Hours (81.3 PIREPs)\n\t+2262.0 Points (11.7 PIREPs)\nCaptain:\n\t+190.0 Hours (171.5 PIREPs)\n\t+8262.0 Points (42.8 PIREPs)\n\t+1000.0 Bonus Points\nSenior Captain:\n\t+440.0 Hours (397.2 PIREPs)\n\t+23262.0 Points (120.5 PIREPs)\n\t+1500.0 Bonus Points\nBase Captain:\n\t+540.0 Hours (487.4 PIREPs)\n\t+48262.0 Points (249.9 PIREPs)\n\t+2500.0 Bonus Points\nTime mode: air",
         "sortName": "Delta"
     },
     "1131": {
+        "callsigns": [
+            "GBG"
+        ],
         "name": "Air Goldberg",
         "rank_info": "Second Officer:\n\t+47.9 Hours (45.9 PIREPs)\n\t+3703.0 Points (24.9 PIREPs)\nFirst Officer:\n\t+147.9 Hours (141.6 PIREPs)\n\t+13703.0 Points (92.3 PIREPs)\nSenior First Officer:\n\t+297.9 Hours (285.1 PIREPs)\n\t+27203.0 Points (183.2 PIREPs)\nCaptain:\n\t+597.9 Hours (572.2 PIREPs)\n\t+59703.0 Points (402.0 PIREPs)\nTraining Captain:\n\t+997.9 Hours (955.1 PIREPs)\n\t+99703.0 Points (671.4 PIREPs)\nTime mode: block",
         "sortName": "Goldberg"
     },
     "1259": {
+        "callsigns": [
+            "AAL",
+            "ACA",
+            "ACV",
+            "DAL",
+            "FDX",
+            "JZA",
+            "ROU",
+            "SWA",
+            "UAL",
+            "WJA"
+        ],
         "name": "Air Canada",
         "rank_info": "Bonus points not included in normal points total\nSecond Officer:\n\t+39.8 Hours (19.6 PIREPs)\n\t+2931.0 Points (13.7 PIREPs)\nFirst Officer:\n\t+139.8 Hours (68.8 PIREPs)\n\t+12931.0 Points (60.5 PIREPs)\nSenior First Officer:\n\t+289.8 Hours (142.6 PIREPs)\n\t+26431.0 Points (123.6 PIREPs)\nCaptain:\n\t+589.8 Hours (290.2 PIREPs)\n\t+58931.0 Points (275.6 PIREPs)\nTraining Captain:\n\t+989.8 Hours (486.9 PIREPs)\n\t+98931.0 Points (462.7 PIREPs)\nBase Captain:\n\t+1989.8 Hours (978.9 PIREPs)\n\t+198931.0 Points (930.5 PIREPs)\nTime mode: block",
         "sortName": "Canada"
     },
     "1309": {
+        "callsigns": [
+            "AAY"
+        ],
         "name": "Allegiant",
         "rank_info": "Trainee Second Officer:\n\t+12.7 Hours (16.7 PIREPs)\n\t+2335.0 Points (10.5 PIREPs)\nSecond Officer:\n\t+37.7 Hours (49.7 PIREPs)\n\t+7335.0 Points (33.1 PIREPs)\nSenior Second Officer:\n\t+62.7 Hours (82.6 PIREPs)\n\t+12335.0 Points (55.6 PIREPs)\nTrainee First Officer:\n\t+87.7 Hours (115.5 PIREPs)\n\t+19335.0 Points (87.2 PIREPs)\nFirst Officer:\n\t+112.7 Hours (148.4 PIREPs)\n\t+24335.0 Points (109.8 PIREPs)\nSenior First Officer:\n\t+137.7 Hours (181.3 PIREPs)\n\t+29335.0 Points (132.3 PIREPs)\nCaptain:\n\t+162.7 Hours (214.2 PIREPs)\n\t+34335.0 Points (154.9 PIREPs)\nJunior Training Captain:\n\t+187.7 Hours (247.1 PIREPs)\n\t+39335.0 Points (177.5 PIREPs)\nTraining Captain:\n\t+212.7 Hours (280.0 PIREPs)\n\t+44335.0 Points (200.0 PIREPs)\nSenior Training Captain:\n\t+237.7 Hours (312.9 PIREPs)\n\t+49335.0 Points (222.6 PIREPs)\nTime mode: air",
         "sortName": "Allegiant"
