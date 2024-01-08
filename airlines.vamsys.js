2b6657ac0a74a135a21c2964dce22467c1e2eea3
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2024-01-08 18:21:59 +0000

    EIN.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -49,8 +49,8 @@ const airlines = {
             "EUK"
         ],
         "name": "Aer Lingus",
-        "rank_info": "First Officer:\n\t+3.6 Hours (2.5 PIREPs)\n\t+440.0 Points (2.0 PIREPs)\nSenior First Officer:\n\t+73.6 Hours (50.3 PIREPs)\n\t+7940.0 Points (35.2 PIREPs)\nCaptain:\n\t+273.6 Hours (186.8 PIREPs)\n\t+25940.0 Points (115.0 PIREPs)\n\t+2400.0 Bonus Points (27.0 PIREPs)\nSenior Captain (TRE/TRI/Line):\n\t+873.6 Hours (596.3 PIREPs)\n\t+70940.0 Points (314.5 PIREPs)\n\t+10400.0 Bonus Points (117.0 PIREPs)\nBase Chief Pilot:\n\t+1473.6 Hours (1005.8 PIREPs)\n\t+125940.0 Points (558.4 PIREPs)\n\t+18400.0 Bonus Points (207.0 PIREPs)\nTime mode: air",
-        "requirements": "1 PIREP(s) required over 30 days\nNext 1 PIREP(s) required by 2024-02-04",
+        "rank_info": "First Officer:\n\t+2.6 Hours (1.8 PIREPs)\n\t+235.0 Points (1.0 PIREPs)\nSenior First Officer:\n\t+72.6 Hours (50.3 PIREPs)\n\t+7735.0 Points (34.5 PIREPs)\nCaptain:\n\t+272.6 Hours (189.0 PIREPs)\n\t+25735.0 Points (114.6 PIREPs)\n\t+2000.0 Bonus Points (19.0 PIREPs)\nSenior Captain (TRE/TRI/Line):\n\t+872.6 Hours (604.9 PIREPs)\n\t+70735.0 Points (315.1 PIREPs)\n\t+10000.0 Bonus Points (95.0 PIREPs)\nBase Chief Pilot:\n\t+1472.6 Hours (1020.8 PIREPs)\n\t+125735.0 Points (560.1 PIREPs)\n\t+18000.0 Bonus Points (171.0 PIREPs)\nTime mode: air",
+        "requirements": "1 PIREP(s) required over 30 days\nNext 1 PIREP(s) required by 2024-02-07",
         "sortName": "Linugs"
     },
     "35": {
