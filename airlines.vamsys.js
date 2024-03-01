84ffd58fd95c9866dd4a2d4fb76d3fc9ef566a29
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2024-03-01 17:53:51 +0000

    DAL.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -261,9 +261,9 @@ const airlines = {
             "REP",
             "SKW"
         ],
-        "last_pirep_start": "2024-02-20T14:59:15.000000Z",
+        "last_pirep_start": "2024-03-01T14:57:36.000000Z",
         "name": "Delta",
-        "rank_info": "First Officer:\n\t+37.7 Hours (33.7 PIREPs)\nSenior First Officer:\n\t+87.7 Hours (78.5 PIREPs)\n\t+1887.0 Points (9.8 PIREPs)\nCaptain:\n\t+187.7 Hours (168.0 PIREPs)\n\t+7887.0 Points (41.1 PIREPs)\n\t+900.0 Bonus Points (99.0 PIREPs)\nSenior Captain:\n\t+437.7 Hours (391.6 PIREPs)\n\t+22887.0 Points (119.1 PIREPs)\n\t+1400.0 Bonus Points (154.0 PIREPs)\nBase Captain:\n\t+537.7 Hours (481.1 PIREPs)\n\t+47887.0 Points (249.3 PIREPs)\n\t+2400.0 Bonus Points (264.0 PIREPs)\nTime mode: air",
+        "rank_info": "First Officer:\n\t+36.3 Hours (31.8 PIREPs)\nSenior First Officer:\n\t+86.3 Hours (75.6 PIREPs)\n\t+1691.0 Points (8.8 PIREPs)\nCaptain:\n\t+186.3 Hours (163.3 PIREPs)\n\t+7691.0 Points (40.0 PIREPs)\n\t+900.0 Bonus Points (108.0 PIREPs)\nSenior Captain:\n\t+436.3 Hours (382.4 PIREPs)\n\t+22691.0 Points (117.9 PIREPs)\n\t+1400.0 Bonus Points (168.0 PIREPs)\nBase Captain:\n\t+536.3 Hours (470.0 PIREPs)\n\t+47691.0 Points (247.9 PIREPs)\n\t+2400.0 Bonus Points (288.0 PIREPs)\nTime mode: air",
         "sortName": "Delta"
     },
     "1131": {
