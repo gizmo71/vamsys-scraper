2e08d12ec099d24f663d63357afe8854ec7605c0
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2024-08-01 16:17:53 +0100

    KLM/AFR.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -30,9 +30,9 @@ const airlines = {
             "TRA",
             "TVF"
         ],
-        "last_pirep_start": "2024-07-18T13:33:37.000000Z",
+        "last_pirep_start": "2024-08-01T13:42:35.000000Z",
         "name": "Air France-KLM",
-        "rank_info": "First Officer:\n\t+0.7 Hours (2.3 PIREPs)\nSenior First Officer:\n\t+249.7 Hours (820.2 PIREPs)\n\t+24815.0 Points (134.1 PIREPs)\nCaptain:\n\t+499.7 Hours (1641.3 PIREPs)\n\t+49815.0 Points (269.3 PIREPs)\nSenior Captain:\n\t+999.7 Hours (3283.7 PIREPs)\n\t+99815.0 Points (539.5 PIREPs)\nTime mode: air",
+        "rank_info": "First Officer:\n\t+0.1 Hours (0.2 PIREPs)\nSenior First Officer:\n\t+249.1 Hours (546.8 PIREPs)\n\t+24630.0 Points (133.1 PIREPs)\nCaptain:\n\t+499.1 Hours (1095.6 PIREPs)\n\t+49630.0 Points (268.3 PIREPs)\nSenior Captain:\n\t+999.1 Hours (2193.1 PIREPs)\n\t+99630.0 Points (538.5 PIREPs)\nTime mode: air",
         "sortName": "France-KLM"
     },
     "9": {
