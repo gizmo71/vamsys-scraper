0cb3fe74b49582a1ef582b165bbf8b53416a1b57
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2024-12-31 12:39:44 +0000

    WZZ.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -157,7 +157,7 @@ const airlines = {
     },
     "42": {
         "callsigns": [],
-        "last_pirep_start": "2024-11-24 10:00:00",
+        "last_pirep_start": "2024-12-31 11:00:00",
         "name": "Wizz",
         "rank_info": [
             "Block Time"
