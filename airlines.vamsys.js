1dbfcd6896ec5914a79a16dcc212c70e8e175794
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2025-04-10 19:17:00 +0100

    DAN.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -157,14 +157,14 @@ const airlines = {
     },
     "356": {
         "callsigns": [],
-        "last_pirep_start": "2025-03-23 14:28:00",
+        "last_pirep_start": "2025-04-10 16:48:00",
         "name": "Dan Air",
         "rank_info": [
             "Airborne Time"
         ],
         "requirements": {
             "details": "1 PIREP(s) required over 365 days",
-            "target_date": "2026-03-23T14:28:00"
+            "target_date": "2026-04-10T16:48:00"
         },
         "sortName": "Dan Air"
     },
