e70320824733c0ffe7f46030089c00eb76b6c3f2
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2025-08-06 15:39:27 +0100

    AIC.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -182,7 +182,7 @@ const airlines = {
     },
     "876": {
         "callsigns": [],
-        "last_pirep_start": "2025-07-13 09:40:41",
+        "last_pirep_start": "2025-08-06 05:50:40",
         "name": "Air India",
         "rank_info": "Airborne Time",
         "sortName": "India"
