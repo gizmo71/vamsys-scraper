d50375f57b0a639d92cf5a7fde8e04126f439460
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2025-09-20 10:07:05 +0100

    CCA.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -75,7 +75,7 @@ const airlines = {
     },
     "265": {
         "callsigns": [],
-        "last_pirep_start": "2025-08-16 08:53:41",
+        "last_pirep_start": "2025-09-20 08:53:17",
         "name": "Air China",
         "rank_info": "Block Time",
         "sortName": "China"
