00723616986597376bb74795101c33fda20f051a
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2025-10-29 20:43:46 +0000

    EZY.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -1,7 +1,7 @@
 const airlines = {
     "1": {
         "callsigns": [],
-        "last_pirep_start": "2025-10-19 08:20:15",
+        "last_pirep_start": "2025-10-29 20:30:35",
         "name": "EasyJet",
         "rank_info": "Block Time",
         "sortName": "EasyJet"
