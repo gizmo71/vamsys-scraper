46a787dfecf32e3a41dcce9d88c89650f2d0d3ea
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2025-11-30 14:20:41 +0000

    BAW.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -107,7 +107,7 @@ const airlines = {
     },
     "35": {
         "callsigns": [],
-        "last_pirep_start": "2025-11-07 17:19:51",
+        "last_pirep_start": "2025-11-30 13:56:51",
         "name": "British Airways",
         "rank_info": "Block Time",
         "sortName": "British Airways"
