f75a1ee7f8e22894a7df252865231ec9ebcd164c
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2025-12-09 14:33:32 +0000

    FFT.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -200,7 +200,7 @@ const airlines = {
     },
     "939": {
         "callsigns": [],
-        "last_pirep_start": "2025-11-05 18:16:43",
+        "last_pirep_start": "2025-12-09 14:12:08",
         "name": "Frontier",
         "rank_info": "Block Time",
         "sortName": "Frontier"
