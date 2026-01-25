27f62547523d8621f8b2c0a73c0d17c1af039306
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-01-25 10:28:08 +0000

    AWC.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -132,12 +132,12 @@ const airlines = {
     },
     "500": {
         "callsigns": [],
-        "last_pirep_start": "2026-01-24 10:43:00",
+        "last_pirep_start": "2026-01-25 10:12:00",
         "name": "Titan",
         "rank_info": "Block Time",
         "requirements": {
             "details": "1 PIREP(s) required over 360 days",
-            "target_date": "2027-01-19T10:43:00"
+            "target_date": "2027-01-20T10:12:00"
         },
         "sortName": "Titan"
     },
