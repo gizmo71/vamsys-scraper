7c0145670a2dfcab9a5164fe0bdcd08dc1a4594f
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-03-14 12:17:53 +0000

    TCX & DAN.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -22,7 +22,7 @@ const airlines = {
     },
     "13": {
         "callsigns": [],
-        "last_pirep_start": "2026-02-04 16:57:00",
+        "last_pirep_start": "2026-03-14 09:27:00",
         "name": "vTCXgroup",
         "rank_info": "Block Time",
         "sortName": "Thomas Cook"
@@ -107,12 +107,12 @@ const airlines = {
     },
     "356": {
         "callsigns": [],
-        "last_pirep_start": "2026-03-06 13:14:00",
+        "last_pirep_start": "2026-03-14 12:03:00",
         "name": "Dan Air",
         "rank_info": "Air Time",
         "requirements": {
             "details": "1 PIREP(s) required over 365 days",
-            "target_date": "2027-03-06T13:14:00"
+            "target_date": "2027-03-14T12:03:00"
         },
         "sortName": "Dan Air"
     },
