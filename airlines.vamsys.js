c26e2ba75127d7d471c7479fab9ab9799db5bab4
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-05-16 21:59:55 +0100

    TCX.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -22,7 +22,7 @@ const airlines = {
     },
     "13": {
         "callsigns": [],
-        "last_pirep_start": "2026-04-16 16:17:00",
+        "last_pirep_start": "2026-05-16 09:18:00",
         "name": "vTCXgroup",
         "rank_info": "Block Time",
         "sortName": "Thomas Cook"
