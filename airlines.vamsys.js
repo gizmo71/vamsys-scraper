d1b6253b0a9c2f9141eec0c47d8ad7875189112e
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-05-20 08:31:18 +0100

    KLM (pending review for rejected Asobo/ini KLM A21N)

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -168,7 +168,7 @@ const airlines = {
     },
     "8": {
         "callsigns": [],
-        "last_pirep_start": "2026-04-22 16:35:00",
+        "last_pirep_start": "2026-05-20 07:18:00",
         "name": "Air France-KLM",
         "rank_info": "Block Time",
         "sortName": "France-KLM"
