2b9268fc4fa38492fc850797e33646348b1936b2
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-05-20 14:41:35 +0100

    IGO.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -161,7 +161,7 @@ const airlines = {
     },
     "706": {
         "callsigns": [],
-        "last_pirep_start": "2026-04-03 13:49:00",
+        "last_pirep_start": "2026-05-20 13:26:00",
         "name": "IndiGo",
         "rank_info": "Block Time",
         "sortName": "IndiGo"
