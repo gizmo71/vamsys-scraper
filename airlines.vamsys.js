af0904988f1df9cf9d78d35aba84c861bbf9f180
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-05-23 10:35:32 +0100

    ANZ.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -43,7 +43,7 @@ const airlines = {
     },
     "1642": {
         "callsigns": [],
-        "last_pirep_start": "2026-04-19 13:26:00",
+        "last_pirep_start": "2026-05-23 08:22:00",
         "name": "Air New Zealand",
         "rank_info": "Block Time",
         "sortName": "New Zealand"
