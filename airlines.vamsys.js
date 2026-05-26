bb74a47ccf4d91daf92607c4ad5e99bce69a1f3d
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-05-26 20:47:37 +0100

    KLM.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -168,7 +168,7 @@ const airlines = {
     },
     "8": {
         "callsigns": [],
-        "last_pirep_start": "2026-05-20 07:18:00",
+        "last_pirep_start": "2026-05-26 16:41:00",
         "name": "Air France-KLM",
         "rank_info": "Block Time",
         "sortName": "France-KLM"
