521a61b70efa2f1e76aab9909d60302b0afeb80e
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-07-02 11:58:20 +0100

    DLH.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -154,7 +154,7 @@ const airlines = {
     },
     "64": {
         "callsigns": [],
-        "last_pirep_start": "2026-06-04 16:08:00",
+        "last_pirep_start": "2026-07-02 09:42:00",
         "name": "Lufthansa",
         "rank_info": "Air Time",
         "sortName": "Lufthansa"
