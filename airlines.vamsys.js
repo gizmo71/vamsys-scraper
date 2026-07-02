e478e459eba8f3e82b6890d18571cf9dffb596e5
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-07-02 15:11:54 +0100

    RYR.

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -82,7 +82,7 @@ const airlines = {
     },
     "3": {
         "callsigns": [],
-        "last_pirep_start": "2026-06-17 07:55:00",
+        "last_pirep_start": "2026-07-02 13:25:00",
         "name": "Ryanair",
         "rank_info": "Air Time",
         "sortName": "Ryanair"
