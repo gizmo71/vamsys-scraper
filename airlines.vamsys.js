1244e0e25c7fe3c923e2a293c8649348f6ca72a5
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2026-08-20 09:46:44 +0100

    LNI

--- airlines.vamsys.js
+++ airlines.vamsys.js
@@ -118,7 +118,7 @@ const airlines = {
     },
     "4262": {
         "callsigns": [],
-        "last_pirep_start": "2026-07-27 09:23:00",
+        "last_pirep_start": "2026-08-20 08:03:00",
         "name": "Lion Group",
         "rank_info": "Air Time",
         "sortName": "Lion Group"
